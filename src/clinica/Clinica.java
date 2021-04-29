c6bad13ce62fa4d3af7172eb7a4c8470222d0534
cozardi <cozardi>
2021-04-28 22:58:08 -0300

    Proyecto base

---
+++ src/clinica/Clinica.java
@@ -0,0 +1,12 @@
+package clinica;
+
+public class Clinica 
+{
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	private String ciudad;
+	
+	
+	
+}
---
+++ src/clinica/Persona.java
@@ -0,0 +1,12 @@
+package clinica;
+
+public class Persona 
+{
+	private String nombre;
+	private String dni;
+	private String telefono;
+	private String domicilio;
+	private String ciudad;
+	
+	//TODO: getters con setters que checkeen la info (p. ej. digitos de telefono)
+}
