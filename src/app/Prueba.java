868fd81916d892a522b5098032361124c67685d7
JoaquinFioriti <70446526+JoaquinFioriti@users.noreply.github.com>
2021-05-09 20:08:50 -0300

    Main

--- src/app/Prueba.java
+++ src/app/Prueba.java
@@ -1,5 +1,6 @@
 package app;
 
+import clinica.Clinica;
 import exceptions.NoExisteException;
 import usuarios.IMedico;
 import usuarios.MedicoFactory;
@@ -7,14 +8,23 @@ import usuarios.MedicoFactory;
 public class Prueba {
 
     public static void main(String[] args) {
-        IMedico medico;
-            try {
-                medico = MedicoFactory.getMedico("234565","Marconi 2345","Mar del Plata","223456732","Mario Pergollini",1406,3000,"Marianito","Permanente");
-                System.out.println(medico.getHonorario());
-            } catch (NoExisteException e) {
-                System.out.println(e.getMessage());
-            }
-
+        // Creamos la Clinica
+        Clinica clinica = Clinica.getInstance("Hospital", "Avenida siempre viva", "123123", "Balcarce");
+        // Creacion de medicos
+        clinica.addMedico("234565", "Marconi 2345", "Mar del Plata", "223456732", "Debora Melo", 1406, 3000, "Clinico",
+                "Permanente");
+        clinica.addMedico("82216", "Moreno 4562", "Mar del Plata", "6872312", "Monica Galindo", 1407, 5000, "Cirujano",
+                "Temporario");
+        clinica.addMedico("7432138", "Belgrano 2135", "Mar del Plata", "5761234", "Elba Calao", 1408, 6000, "Pediatra",
+                "Permanente");
+        clinica.addMedico("867321", "Colon 1235", "Mar del Plata", "57658432", "Benito Camelo", 1409, 7000, "Clinico",
+                "Temporario");
+        // Creamos un par de pacientes
+        clinica.ingresaPaciente("65761321", "Moreno 1239", "Mar del plata", "2234564687", "Verconica Galindo", "Nino");
+        clinica.ingresaPaciente("7561238", "Colon 1239", "Mar del plata", "6873213", "Elmer Melada", "joven");
+        clinica.ingresaPaciente("1354314", "Buenos Aires 1239", "Mar del plata", "2234564687",
+                "Elmer Idiano de Greenwich", "Nino");
+        // Ingreso del paciente a la clínica
 
     }
 }
--- src/clinica/Clinica.java
+++ src/clinica/Clinica.java
@@ -11,32 +11,33 @@ import usuarios.Paciente;
 import usuarios.PacienteFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeSet;
 
-
 /**
- * Esta clase contiene la informacion de los medicos, los pacientes y donde se encuentran los mismos
+ * Esta clase contiene la informacion de los medicos, los pacientes y donde se
+ * encuentran los mismos
  */
 
-public class Clinica 
-{
+public class Clinica {
 	private static Clinica instance = null;
-	//DATOS DE LA CLINICA
-	private String nombre,direccion,telefono,ciudad;
+	// DATOS DE LA CLINICA
+	private String nombre, direccion, telefono, ciudad;
 
-	private Set<IMedico> medicos;
-	private Set<Paciente> pacientes;
+	private Set<IMedico> medicos = new HashSet<IMedico>();
+	private Set<Paciente> pacientes = new HashSet<Paciente>();
 	private PriorityQueue<Paciente> listaEspera;
 	private ArrayList<Paciente> pacientesEnAtencion;
 
 	/**
 	 * Constructor privado, es invocado por el metodo <i>getInstance</i>
-	 * @param nombre Representa el nombre de la clinica
+	 * 
+	 * @param nombre    Representa el nombre de la clinica
 	 * @param direccion Contiene la direccion de la clinica
-	 * @param telefono Contiene el telefono de la clinica
-	 * @param ciudad Contiene la ciudad donde se encuentra la clinica
+	 * @param telefono  Contiene el telefono de la clinica
+	 * @param ciudad    Contiene la ciudad donde se encuentra la clinica
 	 */
 	private Clinica(String nombre, String direccion, String telefono, String ciudad) {
 		this.nombre = nombre;
@@ -53,86 +54,106 @@ public class Clinica
 	}
 
 	/**
-	 * Crea la instancia de la clinica en caso de que esta no exista, sino devuelve la existente
-	 * @param nombre Representa el nombre de la clinica
+	 * Crea la instancia de la clinica en caso de que esta no exista, sino devuelve
+	 * la existente
+	 * 
+	 * @param nombre    Representa el nombre de la clinica
 	 * @param direccion Contiene la direccion de la clinica
-	 * @param telefono Contiene el telefono de la clinica
-	 * @param ciudad Contiene la ciudad donde se encuentra la clinica
+	 * @param telefono  Contiene el telefono de la clinica
+	 * @param ciudad    Contiene la ciudad donde se encuentra la clinica
 	 * @return Una instancia de la clinica
 	 */
-	public static Clinica getInstance(String nombre, String direccion, String telefono, String ciudad){
+	public static Clinica getInstance(String nombre, String direccion, String telefono, String ciudad) {
 		if (instance == null)
-			instance = new Clinica(nombre,direccion,telefono,ciudad);
+			instance = new Clinica(nombre, direccion, telefono, ciudad);
 		return instance;
 	}
 
 	/**
-	 * <b>Pre: </b> Deberia invocarse anteriormente al otro constructor para generar la primer instancia de la clinica, luego usar constructor sin parametros.
+	 * <b>Pre: </b> Deberia invocarse anteriormente al otro constructor para generar
+	 * la primer instancia de la clinica, luego usar constructor sin parametros.
+	 * 
 	 * @return La instancia previamente creada (<i>null</i> en caso de no existir).
 	 */
-	public static Clinica getInstance(){
+	public static Clinica getInstance() {
 		return instance;
 	}
 
-
 	/**
-	 * Agrega un medico a la lista de medicos de la Clinica, en caso de ingresar un dato erroneo informa con un mensaje y no crea el objeto
-	 * @param dni Contiene el DNI del medico
-	 * @param domicilio contiene el domicilio del medico
-	 * @param ciudad representa la ciudad de origen
-	 * @param telefono contiene el telefono del medico
-	 * @param nombre Nombre y apellido del medico
-	 * @param numero representa el numero de licencia medico (unico por medico)
-	 * @param honorarioBasico Honorario basico del medico
-	 * @param especialidad especialidad del medico (clinico, cirujano, pediatra)
+	 * Agrega un medico a la lista de medicos de la Clinica, en caso de ingresar un
+	 * dato erroneo informa con un mensaje y no crea el objeto
+	 * 
+	 * @param dni              Contiene el DNI del medico
+	 * @param domicilio        contiene el domicilio del medico
+	 * @param ciudad           representa la ciudad de origen
+	 * @param telefono         contiene el telefono del medico
+	 * @param nombre           Nombre y apellido del medico
+	 * @param numero           representa el numero de licencia medico (unico por
+	 *                         medico)
+	 * @param honorarioBasico  Honorario basico del medico
+	 * @param especialidad     especialidad del medico (clinico, cirujano, pediatra)
 	 * @param tipoContratacion tipo de contratacion (permanente, temporario)
 	 */
-	public void addMedico(String dni, String domicilio, String ciudad, String telefono, String nombre, int numero,int honorarioBasico,String especialidad,String tipoContratacion){
-		try{
-			IMedico medico = MedicoFactory.getMedico(dni,domicilio,ciudad,telefono,nombre,numero,honorarioBasico,especialidad,tipoContratacion);
+	public void addMedico(String dni, String domicilio, String ciudad, String telefono, String nombre, int numero,
+			int honorarioBasico, String especialidad, String tipoContratacion) {
+		try {
+			IMedico medico = MedicoFactory.getMedico(dni, domicilio, ciudad, telefono, nombre, numero, honorarioBasico,
+					especialidad, tipoContratacion);
+			medicos.add(medico);// tira error aca?-->si
 		} catch (NoExisteException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
 	/**
-	 * Agrega un medico a la lista de medicos de la Clinica, en caso de ingresar un dato erroneo informa con un mensaje y no crea el objeto
-	 * @param dni Contiene el DNI del medico
-	 * @param domicilio contiene el domicilio del medico
-	 * @param ciudad representa la ciudad de origen
-	 * @param telefono contiene el telefono del medico
-	 * @param nombre Nombre y apellido del medico
-	 * @param numero representa el numero de licencia medico (unico por medico)
-	 * @param honorarioBasico Honorario basico del medico
-	 * @param especialidad especialidad del medico (clinico, cirujano, pediatra)
+	 * Agrega un medico a la lista de medicos de la Clinica, en caso de ingresar un
+	 * dato erroneo informa con un mensaje y no crea el objeto
+	 * 
+	 * @param dni              Contiene el DNI del medico
+	 * @param domicilio        contiene el domicilio del medico
+	 * @param ciudad           representa la ciudad de origen
+	 * @param telefono         contiene el telefono del medico
+	 * @param nombre           Nombre y apellido del medico
+	 * @param numero           representa el numero de licencia medico (unico por
+	 *                         medico)
+	 * @param honorarioBasico  Honorario basico del medico
+	 * @param especialidad     especialidad del medico (clinico, cirujano, pediatra)
 	 * @param tipoContratacion tipo de contratacion (permanente, temporario)
-	 * @param posgrado posgrado del medico en caso de tenerlo (magister, doctorado)
+	 * @param posgrado         posgrado del medico en caso de tenerlo (magister,
+	 *                         doctorado)
 	 */
-	public void addMedico(String dni, String domicilio, String ciudad, String telefono, String nombre, int numero,int honorarioBasico,String especialidad,String tipoContratacion,String posgrado){
-		try{
-			IMedico medico = MedicoFactory.getMedico(dni,domicilio,ciudad,telefono,nombre,numero,honorarioBasico,especialidad,tipoContratacion,posgrado);
+	public void addMedico(String dni, String domicilio, String ciudad, String telefono, String nombre, int numero,
+			int honorarioBasico, String especialidad, String tipoContratacion, String posgrado) {
+		try {
+			IMedico medico = MedicoFactory.getMedico(dni, domicilio, ciudad, telefono, nombre, numero, honorarioBasico,
+					especialidad, tipoContratacion, posgrado);
 			medicos.add(medico);
 		} catch (NoExisteException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-
 	/**
-	 * Ingresa el paciente a la clinica y lo deriva a la sala de espera. En caso de no poder crearlo informa el error con un mensaje.
-	 * @param dni String, contiene el DNI del paciente. <br>
-	 * @param domicilio String, contiene el Domicilio en nombre y número. <br>
-	 * @param ciudad String, representa la ciudad de nacimiento. <br>
-	 * @param telefono String, contiene el numero de telefono del paciente. <br>
-	 * @param nombre Nombre y Apellido del paciente. <br>
-	 * @param numHistClinica valor autoincremental asignado por la clinica, representa su numero de Historia Clinica. <br>
-	 * @param rangoEtario Rango etario del paciente (nino, joven o mayor) <br>
+	 * Ingresa el paciente a la clinica y lo deriva a la sala de espera. En caso de
+	 * no poder crearlo informa el error con un mensaje.
+	 * 
+	 * @param dni            String, contiene el DNI del paciente. <br>
+	 * @param domicilio      String, contiene el Domicilio en nombre y número. <br>
+	 * @param ciudad         String, representa la ciudad de nacimiento. <br>
+	 * @param telefono       String, contiene el numero de telefono del paciente.
+	 *                       <br>
+	 * @param nombre         Nombre y Apellido del paciente. <br>
+	 * @param numHistClinica valor autoincremental asignado por la clinica,
+	 *                       representa su numero de Historia Clinica. <br>
+	 * @param rangoEtario    Rango etario del paciente (nino, joven o mayor) <br>
 	 */
 
-	public void ingresaPaciente(String dni, String domicilio, String ciudad, String telefono, String nombre, int numHistClinica,String rangoEtario){
+	public void ingresaPaciente(String dni, String domicilio, String ciudad, String telefono, String nombre,
+			String rangoEtario) {
 		try {
-			Paciente paciente = PacienteFactory.getPaciente(dni,domicilio,ciudad,telefono,nombre,rangoEtario);
-			pacientes.add(paciente);    //si el paciente ya se encuentra no se agrega a la estructura (por implementar la interfaz Set)
+			Paciente paciente = PacienteFactory.getPaciente(dni, domicilio, ciudad, telefono, nombre, rangoEtario);
+			pacientes.add(paciente); // si el paciente ya se encuentra no se agrega a la estructura (por implementar
+										// la interfaz Set)
 			listaEspera.add(paciente);
 			SalaDeEspera.getinstance().ingresaSala(paciente);
 		} catch (NoExisteException e) {
@@ -141,80 +162,72 @@ public class Clinica
 	}
 
 	/**
-	 * Mueve el paciente de la lista de espera, lo retira de la sala de espera correspondiente y lo ingresa en la lista de pacientes en atencion.<br>
-	 * En caso de no haber pacientes en la lista de espera, no realiza ningun movimiento.
+	 * Mueve el paciente de la lista de espera, lo retira de la sala de espera
+	 * correspondiente y lo ingresa en la lista de pacientes en atencion.<br>
+	 * En caso de no haber pacientes en la lista de espera, no realiza ningun
+	 * movimiento.
 	 */
-	public void atiendePaciente(){
+	public void atiendePaciente() {
 		Paciente paciente = listaEspera.poll();
 		if (paciente != null) {
 			SalaDeEspera.getinstance().retiraPaciente(paciente);
 			pacientesEnAtencion.add(paciente);
 		}
 	}
-	
+
 	/**
 	 * Busca el paciente con cierto numero de paciente
 	 * 
 	 * @param numeroPaciente: El numero de historia clinica del paciente a buscar
 	 * @return Retorna el paciente con ese numero o null si no existe
 	 */
-	private Paciente buscaPaciente(int numeroPaciente)
-	{
-		for (Paciente paciente : pacientesEnAtencion)
-		{
-			if (paciente.getNumero() == numeroPaciente)
-			{
+	private Paciente buscaPaciente(int numeroPaciente) {
+		for (Paciente paciente : pacientesEnAtencion) {
+			if (paciente.getNumero() == numeroPaciente) {
 				return paciente;
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Agrega una consulta m�dica al paciente
 	 * 
-	 * <b>pre:</b> El paciente y medico dados no son nulos
-     * <b>post:</b> La consulta queda registrada en el paciente y el reporte del m�dico
-     * 
+	 * <b>pre:</b> El paciente y medico dados no son nulos <b>post:</b> La consulta
+	 * queda registrada en el paciente y el reporte del m�dico
+	 * 
 	 * @param paciente: Paciente que recibio la consulta
-	 * @param medico: Medico que hizo la consulta
+	 * @param medico:   Medico que hizo la consulta
 	 * @throws PacienteInvalidoException si el medico o paciente son nulos
 	 */
-	public void agregaConsultaAPaciente(Paciente paciente, Medico medico) throws Exception
-	{
-		//TODO: integrar con m�dulo de Reporte de Actividad Medica
-		if (paciente != null)
-		{
+	public void agregaConsultaAPaciente(Paciente paciente, Medico medico) throws Exception {
+		// TODO: integrar con m�dulo de Reporte de Actividad Medica
+		if (paciente != null) {
 			paciente.AgregaConsulta(medico);
-		}
-		else
-		{
+		} else {
 			throw new PacienteInvalidoException("Error. Paciente es nulo");
 		}
 	}
-	
-	
+
 	/**
 	 * Agrega una estad�a en una habitacion al paciente<br>
 	 * 
 	 * <b>pre:</b> El paciente y medico dados no son nulos<br>
-     * <b>post:</b> La consulta queda registrada en el paciente y el reporte del m�dico<br>
-     * 
-	 * @param paciente: Paciente internado
+	 * <b>post:</b> La consulta queda registrada en el paciente y el reporte del
+	 * m�dico<br>
+	 * 
+	 * @param paciente:   Paciente internado
 	 * @param habitacion: Habitacion en la que estuvo el paciente
-	 * @param dias: dias de estadia en la habitacion
+	 * @param dias:       dias de estadia en la habitacion
 	 * @throws PacienteInvalidoException si el medico o paciente son nulos
 	 */
-	public void agregaInternacionAPaciente(Paciente paciente, Habitacion hab, int dias) throws Exception
-	{
+	public void agregaInternacionAPaciente(Paciente paciente, Habitacion hab, int dias) throws Exception {
 
-		if (paciente != null)
-		{
+		if (paciente != null) {
 			paciente.AgregaInternacion(hab, 0);
-		}
-		else
-		{
+		} else {
 			throw new PacienteInvalidoException("Error. Paciente es nulo");
 		}
 	}
+
 }
