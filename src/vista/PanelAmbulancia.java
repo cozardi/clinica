f58f6dc96284f0a28c2ccd66c13bb739cb6ffeb3
mariano.garciae24@gmail.com <mariano.garciae24@gmail.com>
2021-06-27 20:01:15 -0300

    Documentado todo

---
+++ src/vista/PanelAmbulancia.java
@@ -0,0 +1,41 @@
+package vista;
+
+import app.ObserverAmbulanciaPrueba;
+import modelo.ambulancia.Ambulancia;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class PanelAmbulancia extends JPanel implements Observer {
+    ImageIcon foto = null;
+    private JLabel labelFoto;
+    Ambulancia ambulancia;
+
+    public PanelAmbulancia(Ambulancia ambulancia) {
+        setLayout(new GridLayout(1, 0, 0, 0));
+
+        
+        this.labelFoto = new JLabel(Recursos.DISPONIBLE);
+        this.labelFoto.setHorizontalAlignment(SwingConstants.CENTER);
+        add(this.labelFoto);
+        this.ambulancia = ambulancia;
+        this.ambulancia.addObserver(this);
+    }
+
+    public void setFoto(ImageIcon foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if(this.ambulancia == o) {
+            ImageIcon fotoNueva = (ImageIcon) arg;
+           this.labelFoto.setIcon(fotoNueva);
+           this.labelFoto.paint(this.getGraphics());
+        }
+        else
+            throw new IllegalArgumentException();
+    }
+}
---
+++ src/vista/Recursos.java
@@ -0,0 +1,13 @@
+package vista;
+
+import javax.swing.*;
+
+public interface Recursos {
+    ImageIcon DISPONIBLE = new ImageIcon("src/svg/disponible.png");
+    ImageIcon ATENDIENDO_PACIENTE = new ImageIcon("src/svg/atendiendo_paciente.png");
+    ImageIcon REGRESANDO_CLINICA = new ImageIcon("src/svg/regresando_clinica.png");
+    ImageIcon ENTALLER = new ImageIcon("src/svg/entaller.png");
+    ImageIcon REGRESANDO_TALLER = new ImageIcon("src/svg/regresando_taller.png");
+    ImageIcon TRASLADANDO_PACIENTE = new ImageIcon("src/svg/traslando_paciente.png");
+
+}
